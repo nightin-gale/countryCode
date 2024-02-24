d3d7e23d6094a43b5623c9f6dcd16543e94cdabe
Harry Yuhao Han <s2162783@ed.ac.uk>
2024-02-24 12:11:48 +0000

    doc

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+ Package countryCode provides the database for country name and country code.
+ It exposes two functions: GetCountryCode and GetCountryName for database lookup.
+*/
+
+package countryCode
